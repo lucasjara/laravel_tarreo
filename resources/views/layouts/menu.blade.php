6233d2346617537bab0d59bac4d575650e4b89d2
lucasjara <lucasjaraespinoza@gmail.com>
2017-10-09 02:36:19 -0300

    Ajustes visuales

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/menu.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+<div id="app">
+    <nav class="navbar navbar-default navbar-static-top" style="margin-bottom: 0px;">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    E-Students INACAP 2017
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    &nbsp;
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Registro</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                   aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                              style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                            @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div id="wrapper" class="active">
+        <div id="sidebar-wrapper">
+            <ul id="sidebar_menu" class="sidebar-nav">
+                <li class="sidebar-brand"><a id="menu-toggle" href="#">Menu<span id="main_icon"
+                                                                                 class="glyphicon glyphicon-align-justify"></span></a>
+                </li>
+            </ul>
+            <ul class="sidebar-nav" id="sidebar">
+                <li><a>Usuarios<span class="sub_icon glyphicon glyphicon-user"></span></a></li>
+                <ul class="sidebar-nav" id="sidebar">
+                    <li><a>Crear<span class="sub_icon glyphicon glyphicon-link"></span></a></li>
+                    <li><a>Modificar<span class="sub_icon glyphicon glyphicon-pencil"></span></a></li>
+                </ul>
+                <li><a>Puntajes<span class="sub_icon glyphicon glyphicon-star"></span></a></li>
+                <ul class="sidebar-nav" id="sidebar">
+                    <li><a>Agregar<span class="sub_icon glyphicon glyphicon-link"></span></a></li>
+                    <li><a>Modificar<span class="sub_icon glyphicon glyphicon-pencil"></span></a></li>
+                    <li><a>Listado<span class="sub_icon glyphicon glyphicon-list-alt"></span></a></li>
+                </ul>
+                <li><a>Competencias<span class="sub_icon glyphicon glyphicon-fire"></span></a></li>
+            </ul>
+        </div>
+        <div id="page-content-wrapper">
+            <div class="page-content inset">
+                <div class="row">
+                    <br>
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/script_menu.js') }}"></script>
+
+</body>
+</html>
