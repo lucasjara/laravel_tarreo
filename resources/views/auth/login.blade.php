6a5777a3b54a68529a80df14f534095e7cb7033a
lucasjara <lucasjaraespinoza@gmail.com>
2017-10-09 01:35:45 -0300

    retoques texto y agregado de imagen background

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,18 +2,19 @@
 
 @section('content')
     <div class="container">
+        <link href="{{ asset('css/imagen.css') }}" rel="stylesheet">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-primary">
                     <div class="panel-heading">Login</div>
                     <div class="panel-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
                             {{ csrf_field() }}
 
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="col-md-4 control-label">Correo:</label>
+                                <label for="email" class="col-md-2 control-label">Correo:</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-10">
                                     <input id="email" type="email" class="form-control" name="email"
                                            value="{{ old('email') }}" required autofocus>
 
@@ -26,9 +27,9 @@
                             </div>
 
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Contraseña:</label>
+                                <label for="password" class="col-md-2 control-label">Contraseña:</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-10">
                                     <input id="password" type="password" class="form-control" name="password" required>
 
                                     @if ($errors->has('password'))
@@ -40,7 +41,7 @@
                             </div>
 
                             <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
+                                <div class="col-md-4 col-md-offset-2">
                                     <div class="checkbox">
                                         <label>
                                             <input type="checkbox"
@@ -51,7 +52,7 @@
                             </div>
 
                             <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4">
+                                <div class="col-md-7 col-md-offset-2">
                                     <button type="submit" class="btn btn-primary">
                                         Login
                                     </button>
